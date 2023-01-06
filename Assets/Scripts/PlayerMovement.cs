bb2695f654c2d9f284ccbc235308f3a4eb6bd1f4
Raddix1985 <raddix1985@live.co.za>
2023-01-06 07:04:02 +0200

    fix bugs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,14 +8,20 @@ public class PlayerMovement : MonoBehaviour
     Vector2 moveInput;
     Rigidbody2D rb2d;
     Animator anim;
+    CapsuleCollider2D capsuleCollider2D;
 
     [SerializeField] float moveSpeed = 5.0f;
     [SerializeField] float jumpForce = 10.0f;
+    [SerializeField] float climbSpeed = 5.0f;
+
+    float gravityScaleAtStart;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        capsuleCollider2D = GetComponent<CapsuleCollider2D>();
+        gravityScaleAtStart = rb2d.gravityScale;
     }
 
     
@@ -23,6 +29,7 @@ public class PlayerMovement : MonoBehaviour
     {
         Run();
         FlipSprite();
+        ClimbLadder();
     }
 
     void OnMove(InputValue value)
@@ -33,6 +40,8 @@ public class PlayerMovement : MonoBehaviour
 
     void OnJump(InputValue value)
     {
+        if (!capsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) {return;}
+
         if (value.isPressed)
         {
             rb2d.velocity += new Vector2(0f, jumpForce);
@@ -57,7 +66,23 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.localScale = new Vector2(Mathf.Sign(rb2d.velocity.x), 1f);
         }
-
         
+    }
+
+    void ClimbLadder()
+    {
+        if (!capsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing"))) 
+        {
+            rb2d.gravityScale = gravityScaleAtStart;
+            anim.SetBool("isClimbing", false);
+            return; 
+        }
+
+        Vector2 climbVelocity = new Vector2(rb2d.velocity.x, moveInput.y * climbSpeed);
+        rb2d.velocity = climbVelocity;
+        rb2d.gravityScale = 0f;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(rb2d.velocity.y) > Mathf.Epsilon;
+        anim.SetBool("isClimbing", playerHasVerticalSpeed);
     }
 }
